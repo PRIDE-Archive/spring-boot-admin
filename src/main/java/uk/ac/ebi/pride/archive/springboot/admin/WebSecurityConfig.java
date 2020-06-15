a23e2c71325511670670bb3e348f4400b4482c4b
Chakradhar Bandla <chakrabandla@gmail.com>
2020-06-15 15:46:08 +0100

    websecurity fix

--- src/main/java/uk/ac/ebi/pride/archive/springboot/admin/WebSecurityConfig.java
+++ src/main/java/uk/ac/ebi/pride/archive/springboot/admin/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
@@ -61,6 +62,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .key(UUID.randomUUID()
                         .toString())
                 .tokenValiditySeconds(1209600);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        // Allow swagger to be accessed without authentication
+        web.ignoring()
+                .antMatchers("/actuator/**/**");
     }
 
     // Required to provide UserDetailsService for "remember functionality"
